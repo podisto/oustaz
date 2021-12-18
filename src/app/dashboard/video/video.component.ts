948aab67853629527570ffb9e492e32e2482a2ca
ElHadji Omar DIONE <omardione1991@gmail.com>
2021-12-19 00:14:46 +0100

    integration grid videos

--- src/app/dashboard/video/video.component.ts
+++ src/app/dashboard/video/video.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-video',
-  templateUrl: './video.component.html'
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.scss']
 })
 export class VideoComponent implements OnInit {
   constructor() { }
